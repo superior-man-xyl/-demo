1fbeffb01d01bd42c4f3905c800d8a0655aa48a4
superior-man-xyl <15170725069@163.com>
2021-01-26 22:11:59 +0800

    首页切图

--- wechatAPP-mall/pages/index/index.js
+++ wechatAPP-mall/pages/index/index.js
@@ -4,6 +4,50 @@ const WXAPI = require('apifm-wxapi');
 var app=getApp();
 Page({
   data: {
+    categories:[
+      {
+        id:0,
+        name:'买它超市',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/125678/35/5947/4868/5efbf28cEbf04a25a/e2bcc411170524f0.png'
+      },{
+        id:1,
+        name:'数码电器',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/135931/4/3281/5598/5efbf2c0Edbdc82c7/ed9861b4ddfb9f30.png'
+      },{
+        id:2,
+        name:'买它服饰',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/140012/8/1804/3641/5efbf318E38bd5dad/0db99d859ab16ce9.png'
+      },{
+        id:3,
+        name:'买它生鲜',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/129215/21/5978/3618/5efbf344Ebec23ae8/59712d986b10bb0a.png'
+      },{
+        id:4,
+        name:'买它到家',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/116602/7/11200/3796/5efbf375Ebba41029/f07cc166f368fa05.png'
+      },{
+        id:5,
+        name:'充值缴费',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/146390/3/1846/4909/5efbf39aEe5f5f797/300071558a9ab078.png'
+      },
+      {
+        id:6,
+        name:'一毛包邮',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/150351/19/14167/6853/5fed882dE195ef673/b2aa7d67e675baf8.png'
+      },{
+        id:7,
+        name:'领卷',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/113589/24/11332/4897/5efbf3feE705d87db/e5c12d5e943266b9.png'
+      },{
+        id:8,
+        name:'领钱了',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/131663/33/3380/3674/5efbf50fEf79cf314/af4b57d2383e605d.png'
+      },{
+        id:9,
+        name:'买它会员',
+        icon:'https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/123730/37/5924/4189/5efbf567E0a226121/d04df7c74c87cf68.png'
+      }
+    ],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
@@ -16,32 +60,7 @@ Page({
     })
   },
   onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
   getUserInfo(e) {
     console.log(e)
--- wechatAPP-mall/pages/start/start.js
+++ wechatAPP-mall/pages/start/start.js
@@ -1,5 +1,6 @@
 // pages/start/start.js
 const WXAPI = require('apifm-wxapi');
+WXAPI.init('gooking')
 var app=getApp();
 Page({
 
@@ -14,7 +15,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    WXAPI.banners().then(res => {
+      if (res.code == 0) {
+        this.setData({
+          banners: res.data
+        })
+      }
+    })
   },
 gotoindex:function(event){
 wx.switchTab({//注意跳转方式，navigateto不能跳进tabbar页面，其带回退，要用不带回退的另外方法才能跳转
@@ -45,20 +52,8 @@ wx.switchTab({//注意跳转方式，navigateto不能跳进tabbar页面，其带
   /**
    * 生命周期函数--监听页面卸载
    */
-  onLoad: function () {
-    // 启动滑屏效果
-    // 图片数据的来源?
-    WXAPI.banners({
-      //头部图 banner 位
-      //app:表示获取整个应用的，刚开始的
-      // https://api.it120.cc/tz/banner/list?type=app//type=app接口
-      type:'app'
-    })
-    // 一个promise
-    .then(res=>{
-      this.setData({
-        banners:res.data})
-    })
+  onunLoad: function () {
+    
   },
 
   /**
